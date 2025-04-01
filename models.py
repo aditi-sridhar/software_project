0fae2eee4916c63dfbe44c253f12b3b32d0c0caf
Aditi Sridhar <aditi.sridhar9563@gmail.com>
2025-04-01 19:47:38 +0530

    created notifications table, as well as fixed the flash display on book_consultations.html

--- models.py
+++ models.py
@@ -7,11 +7,13 @@ class User(db.Model):
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     appointments = db.relationship('Appointment', back_populates='user')
+    notifications=db.relationship('Notification', back_populates='user')
 
 class Doctor(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String(80), nullable=False)
     appointments = db.relationship('Appointment', back_populates='doctor')
+    notifications=db.relationship('Notification', back_populates='doctor')
 
 class Appointment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,3 +23,16 @@ class Appointment(db.Model):
 
     user = db.relationship('User', back_populates='appointments')
     doctor = db.relationship('Doctor', back_populates='appointments')
+
+class Notification(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.id'), nullable=False)
+    appointment_datetime = db.Column(db.DateTime, nullable=False)
+    message = db.Column(db.String(255), nullable=True)
+    created_datetime=db.Column(db.DateTime, nullable=False)
+
+    user = db.relationship('User', back_populates='notifications')
+    doctor = db.relationship('Doctor', back_populates='notifications')
+
+
